9e50b5ffa2f01a2a450d5cfa4ef095cf00894a6c
OfekLap <Ofek2608@gmail.com>
2024-03-11 23:05:31 +0200

    added title bar

--- website/src/app.js
+++ website/src/app.js
@@ -2,10 +2,13 @@ import PostResource from "./pages/resource-page";
 import LoginPage from "./pages/login-page";
 import MarkdownDisplay from "./components/markdown-display";
 import { useState } from "react";
+import './title-bar/title-bar.css';
 
 export default function App() {
+  let [hideTitleBar, setHideTitleBar] = useState(true);
   return (
     <div>
+    <TitleBar hideTitleBar={hideTitleBar} />
       <PostResource
         resourceData={{
           type: "post",
@@ -16,6 +19,23 @@ export default function App() {
       />
       <LoginPage create />
       <TestMarkdown />
+      <MainView />
+    </div>
+  );
+}
+
+function MainView() {
+  return <div></div>;
+}
+
+function TitleBar({ hideTitleBar }) {
+  return (
+    <div
+      className={"title-bar" + (hideTitleBar ? " title-bar-hide" : "")}
+    >
+      <div>
+
+      </div>
     </div>
   );
 }
